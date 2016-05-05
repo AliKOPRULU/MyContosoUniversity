5b90c4ec30fcd4f5fd86f32c82d4dba822454208
AliKOPRULU <AK@DESKTOP-3N0MD2B>
2016-05-05 16:39:41 +0300

    Kurs, kayıt, öğrenci ve dbcontext eklendi. Çekirdek bilgiler eklendi ve güncelleme yapıldı.

---
+++ MyContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,28 @@
+﻿using MyContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace MyContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {//DataContext
+        public SchoolContext() : base("SchoolContext")
+        {//connection string bu olacak 
+
+        }
+
+        //entity (varlıklarını) burada tanımladık
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {//Tablo isimlerinin s takısı almasını engelledik
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ MyContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,18 @@
+﻿namespace MyContosoUniversity.Models
+{
+    public enum Grade
+    {//Sınıf
+        A, B, C, D, F
+    }
+    public class Enrollment
+    {//Kayıt
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        //Sınıf null olabilir, henüz atanmamış //Çekirdek veriler eklenirken de bazılarına sınıf ataması yapılmamış.
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
